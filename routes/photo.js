515e36c0b0c06a37d295d768820c3e95a1847359
hank <tangxiaojun_123@126.com>
2018-12-23 09:45:12 +0800

    add album view times

--- routes/photo.js
+++ routes/photo.js
@@ -4,13 +4,24 @@ const options = require('../utils/options')
 module.exports = async(req, res, next) => {
   const { album } = req.params
 
-  let aql, vertex, albumKey
+  let aql, vertex, albumKey, result
   
   try {
     albumKey = album
+    //Add view times
+    vertex = options.vertex.album
+    aql = `for p IN ${vertex} 
+      FILTER p._key==@album and p.status==1 
+      update p with {view: p.view+1} IN ${vertex}
+    `
+    result = await arangodb.query(
+      aql, 
+      {album: albumKey}
+    )
+
+    //Retrieve album's photos
     vertex = options.vertex.photo
     aql = `for p IN ${vertex} FILTER p.album==@album and p.status==1 RETURN p`
-
     const data = await arangodb.query(
       aql, 
       {album: albumKey}
